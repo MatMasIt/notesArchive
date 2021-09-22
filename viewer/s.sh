17bc2ffdc5bc489ac8183ee30bf350ec0910d763
MatMasIt <mattia.mascarello@outlook.it>
2021-09-22 10:29:32 +0200

    Reverse output for most recent

--- viewer/s.sh
+++ viewer/s.sh
@@ -1,7 +1,7 @@
 git config core.quotepath off
 rm tempdata
 touch tempdata
-tree -ifFCD --timefmt '%Y%m%d %H%M%S' latest | sort -k1 -k2 | grep "md" | awk '{print($3)}' | tail -n 10 | tee tempdata
+tree -ifFCD --timefmt '%Y%m%d %H%M%S' latest | sort -k1 -k2 | grep "md" | awk '{print($3)}' | tail -n 10 | tac | tee tempdata
 php viewer/jp.php
 rm tempdata
 mv list.json viewer/list.json
