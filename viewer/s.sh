4f0e2db886c1ed1e43dfa6e55a6dd3e6263e427c
MatMasIt <mattia.mascarello@outlook.it>
2021-10-11 13:33:32 +0200

    Delete viewer directory

--- viewer/s.sh
+++
@@ -1,7 +0,0 @@
-git config core.quotepath off
-rm tempdata
-touch tempdata
- git ls-files -z | xargs -0 -n1 -I{} -- git log -1 --format="%ai {}" {} | grep appunti.md | sort | awk '{for (i=4; i<=NF; i++) print $i}' | tail -n 20 | tac | tee tempdata
-php viewer/jp.php
-rm tempdata
-mv list.json viewer/list.json
