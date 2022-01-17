433dd00809fdd3bd10445b66abae1018c74b5d0b
MatMasIt <mattia.mascarello@outlook.it>
2022-01-17 11:59:39 +0100

    Create imageDump.sh

---
+++ imageDump.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+images=$(grep -R -Eo "(http|https)://[a-zA-Z0-9./?=_%:-]*" latest | grep "i.imgur.com" | sort -u)
+mkdir -p imageArchive
+touch imageArchive/list.txt
+echo "$images" >  imageArchive/list.txt
+iList=$(echo "$images" | cut -d: -f2-)
+echo "$iList" | for url in $(cat) ; do
+        filename=$(echo "$url" | awk -F"/" '{print($NF)}')
+        wget -nc $url -O "imageArchive/"$filename
+done
